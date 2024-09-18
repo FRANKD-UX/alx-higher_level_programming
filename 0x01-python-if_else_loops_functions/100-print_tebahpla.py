162b1e1a9d3a25a571b426233c9546389e8a4047
FRANKD-UX <frankdafrica@gmail.com>
2024-09-18 03:23:30 -0700

    g

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
-def print_reverse_alphabet():
-    for i in range(122, 96, -1):
-        print("{}".format(chr(i) if i % 2 == 0 else chr(i - 32)), end="")
+for i in range(122, 96, -1):
+    print("{}".format(chr(i) if i % 2 == 0 else chr(i).upper()), end="")
