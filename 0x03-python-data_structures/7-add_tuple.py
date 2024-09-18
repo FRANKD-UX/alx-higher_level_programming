6bb05e9994f33312f29b529fee165c13a59fde49
FRANKD-UX <frankdafrica@gmail.com>
2024-09-18 06:28:09 -0700

    f

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,4 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     # Ensure both tuples have at least 2 elements by padding with zeros
     tuple_a += (0, 0)
-    tuple_b += (0, 0) 
+    tuple_b += (0, 0)
+
+# Only consider the first two elements of each tuple
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
