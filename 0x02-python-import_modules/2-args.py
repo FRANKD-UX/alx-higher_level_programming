a70fd6d2b61bdb17797d2cb6e64fb214f350476d
FRANKD-UX <frankdafrica@gmail.com>
2024-09-19 06:28:24 -0700

    w

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    argc = len(argv) - 1
+
+    if argc == 0:
+        print("Number of argument(s): 0.")
+    elif argc == 1:
+        print("Number of argument(s): 1 argument:")
+    else:
+        print("Number of argument(s): {} arguments:".format(argc))
+
+    for i in range(1, argc + 1):
+        print("{}: {}".format(i, argv[i]))
