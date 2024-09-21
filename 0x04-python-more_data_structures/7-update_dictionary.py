78937d97a6f17768c6625772938b645e3f004cc2
FRANKD-UX <frankdafrica@gmail.com>
2024-09-21 15:12:58 -0700

    f

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """
+    Replaces or adds a key/value pair in the dictionary.
+
+    Args:
+        a_dictionary (dict): The input dictionary.
+        key (str): The key to be updated or added.
+        value: The value associated with the key.
+
+    Returns:
+        dict: The updated dictionary.
+    """
+    a_dictionary[key] = value
+    return a_dictionary
+
+
 def print_sorted_dictionary(a_dictionary):
     """
     Prints a dictionary with keys sorted in alphabetical order.
