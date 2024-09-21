a9180c88730a510d669f144af9ddbed7680a0f56
FRANKD-UX <frankdafrica@gmail.com>
2024-09-21 16:46:31 -0700

    :r:

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,18 +1,16 @@
 #include <Python.h>
+#include <stdio.h>
 
-/**
- * print_python_list - Prints basic info about Python lists
- * @p: A PyObject list
- */
 void print_python_list(PyObject *p)
 {
 	if (!PyList_Check(p))
 	{
-		printf("[ERROR] Invalid List Object\n");
+		printf("[ERROR] Invalid PyObject: not a list\n");
 		return;
 	}
 
-	Py_ssize_t size = PyList_Size(p);
+	Py_ssize_t size = PyObject_Length(p);
+
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %zd\n", size);
 	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
@@ -20,18 +18,55 @@ void print_python_list(PyObject *p)
 	for (Py_ssize_t i = 0; i < size; i++)
 	{
 		PyObject *item = PyList_GetItem(p, i);
-		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+		const char *type = Py_TYPE(item)->tp_name;
+
+		printf("Element %zd: %s\n", i, type);
 	}
 }
 
-/**
- * print_python_bytes - Prints basic info about Python bytes
- * @p: A PyObject bytes
- */
 void print_python_bytes(PyObject *p)
 {
 	if (!PyBytes_Check(p))
 	{
-		printf("[ERROR] Invalid Bytes Object\n");
+		printf("[ERROR] Invalid PyObject: not a bytes object\n");
 		return;
+	}
+
+	Py_ssize_t size = PyBytes_Size(p);
+
+	printf("[.] bytes object info\n");
+	printf("[.] Size: %zd\n", size);
+	printf("[.] First %zd bytes: ", size > 10 ? 10 : size);
+
+	for (Py_ssize_t i = 0; i < (size > 10 ? 10 : size); i++)
+	{
+		printf("%02x", (unsigned char)PyBytes_AsString(p)[i]);
+		if (i < (size > 10 ? 9 : size - 1))
+			printf(" ");
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	PyObject *list_obj = PyList_New(3);
+	PyObject *bytes_obj = PyBytes_FromStringAndSize("Hello, World!", 13);
+	PyObject *not_a_list = PyUnicode_FromString("Not a list");
+
+	PyList_SetItem(list_obj, 0, PyUnicode_FromString("Element 0"));
+	PyList_SetItem(list_obj, 1, PyLong_FromLong(42));
+	PyList_SetItem(list_obj, 2, PyFloat_FromDouble(3.14));
+
+	print_python_list(list_obj);
+	print_python_bytes(bytes_obj);
+	print_python_list(not_a_list);  /* Should print an error */
+	print_python_bytes(not_a_list);  /* Should print an error */
+
+	/* Cleanup */
+	Py_XDECREF(list_obj);
+	Py_XDECREF(bytes_obj);
+	Py_XDECREF(not_a_list);
+
+	return (0);
+}
 
