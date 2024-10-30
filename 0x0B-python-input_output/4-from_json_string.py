d8fd92ed7a48d02946f1021f572113703cb21749
FRANKD-UX <frankdafrica@gmail.com>
2024-10-30 12:38:21 -0700

    F

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module defines a function that returns an object (Python data structure)
+represented by a JSON string.
+"""
+
+import json
+
+
+gef from_json_string(my_str):
+    """
+    Returns an object (Python data structure) represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string.
+
+    Returns:
+        obj: The object represented by the JSON string.
+    """
+    return json.loads(my_str)
