6a42fdc874906c35b5dbef69577120debf9a0c64
FRANKD-UX <frankdafrica@gmail.com>
2024-10-30 11:55:41 -0700

    F

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,7 +1,13 @@
+#!/usr/bin/python3
+"""
+This script generates Pascal's triangle.
+"""
+
+
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal’s
-    triangle of n.
+    Returns a list of lists of integers representing the
+    Pascal’s triangle of n.
     """
     if n <= 0:
         return []
