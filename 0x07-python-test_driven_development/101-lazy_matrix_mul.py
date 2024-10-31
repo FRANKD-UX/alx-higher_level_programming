ebee5e0ba7e37afcb5afbde623c521a1b1f61a47
FRANKD-UX <frankdafrica@gmail.com>
2024-10-30 22:50:55 -0700

    F

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -24,9 +24,9 @@ def lazy_matrix_mul(m_a, m_b):
         The product of the two matrices.
     """
     if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
     if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
+        raise TypeError("Scalar operands are not allowed, use '*' instead")
 
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
@@ -34,18 +34,25 @@ def lazy_matrix_mul(m_a, m_b):
         raise TypeError("m_b must be a list of lists")
 
     if not all(isinstance(ele, (int, float)) for row in m_a for ele in row):
-        raise TypeError("m_a should contain only integers or floats")
+        raise TypeError("invalid data type for einsum")
     if not all(isinstance(ele, (int, float)) for row in m_b for ele in row):
-        raise TypeError("m_b should contain only integers or floats")
+        raise TypeError("invalid data type for einsum")
 
     if len(m_a) == 0 or len(m_a[0]) == 0:
-        raise ValueError("m_a can't be empty")
+        raise ValueError
+    ("shapes (1,0) and (2,2) not aligned: 0 (dim 1) != 2 (dim 0)")
     if len(m_b) == 0 or len(m_b[0]) == 0:
-        raise ValueError("m_b can't be empty")
+        raise ValueError
+    ("shapes (2,2) and (1,0) not aligned: 2 (dim 1) != 1 (dim 0)")
 
     if not all(len(row) == len(m_a[0]) for row in m_a):
         raise TypeError("each row of m_a must be of the same size")
     if not all(len(row) == len(m_b[0]) for row in m_b):
         raise TypeError("each row of m_b must be of the same size")
 
-    return np.matmul(m_a, m_b)
+    try:
+        result = np.matmul(m_a, m_b)
+    except ValueError as e:
+        raise ValueError("ma and mb can't be multiplied")
+
+    return result
