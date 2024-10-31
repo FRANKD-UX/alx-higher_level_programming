22fd23f1d739042a77eb4321323b66ad6424c026
FRANKD-UX <frankdafrica@gmail.com>
2024-10-30 21:16:25 -0700

    F

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
-This module defines a function that returns an object (Python data structure)
-represented by a JSON string.
+This module provides a function to convert a
+JSON string back to a Python object.
 """
 
 import json
 
 
-gef from_json_string(my_str):
+def from_json_string(my_str):
     """
-    Returns an object (Python data structure) represented by a JSON string.
+    Returns a Python data structure represented by a JSON string.
 
     Args:
-        my_str (str): The JSON string.
+        my_str (str): The JSON string to deserialize.
 
     Returns:
-        obj: The object represented by the JSON string.
+        object: The Python object represented by the JSON string.
     """
     return json.loads(my_str)
