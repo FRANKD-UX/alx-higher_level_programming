ba4f7c4f61c3d6466a5e4c2e2566fd40fe8ae281
FRANKD-UX <frankdafrica@gmail.com>
2024-12-20 19:26:25 -0800

    taks9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -9,7 +9,7 @@ function add (a, b) {
 const args = process.argv.slice(2);
 
 if (args.length !== 2) {
-  console.log('Usage: ./add.js <integer1> <integer2>');
+  console.log('NaN');
   process.exit(1);
 }
 
@@ -18,9 +18,7 @@ const a = parseInt(args[0], 10);
 const b = parseInt(args[1], 10);
 
 if (isNaN(a) || isNaN(b)) {
-  console.log('Both arguments must be integers.');
-  process.exit(1);
+  console.log('NaN');
+} else {
+  console.log(add(a, b));
 }
-
-// Print the result
-console.log(`The sum of ${a} and ${b} is: ${add(a, b)}`);
