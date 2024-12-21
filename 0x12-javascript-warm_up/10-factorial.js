24dd36a4aec9411071274de69003365464974ff0
FRANKD-UX <frankdafrica@gmail.com>
2024-12-20 19:30:50 -0800

    taks10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+function factorial (n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const args = process.argv.slice(2);
+
+const number = parseInt(args[0], 10);
+const result = factorial(isNaN(number) ? 1 : number);
+
+console.log(result);
