2b9c12cace25e4596b2d58339ec3bdf9ace0c8ec
FRANKD-UX <frankdafrica@gmail.com>
2024-12-22 02:05:27 -0800

    taks102

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const file1Path = process.argv[2];
+const file2Path = process.argv[3];
+const destPath = process.argv[4];
+
+const file1Content = fs.readFileSync(file1Path, 'utf8');
+const file2Content = fs.readFileSync(file2Path, 'utf8');
+
+const concatenatedContent = file1Content + file2Content;
+
+fs.writeFileSync(destPath, concatenatedContent);
