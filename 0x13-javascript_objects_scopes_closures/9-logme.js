1b1fd5c7e79bb10ada127858522d073876ea4b29
FRANKD-UX <frankdafrica@gmail.com>
2024-12-22 01:54:00 -0800

    taks9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,6 @@
 let count = 0;
 
-exports.logMe = function(item) {
-    console.log(`${count}: ${item}`);
-    count++;
+exports.logMe = function (item) {
+  console.log(`${count}: ${item}`);
+  count++;
 };
