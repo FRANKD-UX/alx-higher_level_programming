fe5a08229f6791e9a50abb70dc30c78e697317ff
FRANKD-UX <frankdafrica@gmail.com>
2025-01-23 01:19:48 -0800

    task0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status using urllib"""
+
+import urllib.request
+
+def fetch_status():
+    """Fetches and displays the status of a URL"""
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode('utf-8'))
+
+if __name__ == "__main__":
+    fetch_status()
