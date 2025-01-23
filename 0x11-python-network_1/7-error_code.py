5b2d0fd72bfa4458569836d17781c6315bdccf7f
FRANKD-UX <frankdafrica@gmail.com>
2025-01-23 02:18:20 -0800

    taks7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the body of the response or an error code"""
+"""Sends a request to a given URL and displays
+the body of the response or an error code"""
 
 import requests
 import sys
 
 
 def fetch_url(url):
-    """Fetches and displays the body of the response or an error code"""
+    """Fetches and displays the body of the 
+    response or an error code"""
     response = requests.get(url)
     if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
