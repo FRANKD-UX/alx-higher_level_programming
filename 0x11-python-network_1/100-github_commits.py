14a5f8cb7afc3e375ecc64bc004472c1b5d05111
FRANKD-UX <frankdafrica@gmail.com>
2025-01-23 02:28:14 -0800

    task10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Lists 10 commits from a given repository by a given user using the GitHub API"""
+
+import requests
+import sys
+
+
+def list_commits(repo, owner):
+    """Fetches and displays 10 commits from the specified repository"""
+    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    response = requests.get(url)
+    commits = response.json()
+
+    for commit in commits[:10]:
+        sha = commit.get('sha')
+        author = commit.get('commit').get('author').get('name')
+        print(f"{sha}: {author}")
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    list_commits(repo, owner)
