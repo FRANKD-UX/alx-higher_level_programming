d0c1391eeae4feb81f3231ba045393b277cc8ef2
FRANKD-UX <frankdafrica@gmail.com>
2025-01-23 00:06:49 -0800

    taks1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the URL and displays the body of the response if status code is 200
-curl -sL "$1" | grep -q "HTTP/1.1 200 OK" && curl -sL "$1"
+# Sends a GET request to the URL, follows redirects, and displays the body of the response if status code is 200
+curl -sL "$1" | grep -o 'HTTP/1.1 [0-9]*' | grep -q '200' && curl -sL "$1"
