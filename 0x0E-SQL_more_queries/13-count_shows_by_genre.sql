640754f73d0d2453c8c76607e40bf4416e5a6ad1
FRANKD-UX <frankdafrica@gmail.com>
2025-03-09 12:56:16 -0700

    task 13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- List all genres and the number of shows linked to each genre
+SELECT genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
+FROM tv_show_genres
+JOIN genres ON tv_show_genres.genre_id = genres.id
+GROUP BY genre
+HAVING COUNT(tv_show_genres.tv_show_id) > 0
+ORDER BY number_of_shows DESC;
+
