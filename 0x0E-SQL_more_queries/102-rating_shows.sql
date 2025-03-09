14e4347126cfa99b5a1ad506da62714baf0e539a
FRANKD-UX <frankdafrica@gmail.com>
2025-03-09 13:06:37 -0700

    task 102

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- List all shows and their total ratings, sorted by the rating sum in descending order
+SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating
+FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
+
